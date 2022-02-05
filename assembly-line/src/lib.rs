d74284cfbb9842f8ee7fcecf585637dd2c6f10ba
Brad Clements <bkc@murkworks.com>
2022-02-05 16:17:16 -0500

    solution for assembly line.. used type casts

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,11 +1,20 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+const CARS_PER_HOUR: f64 = 221.0;
+const MINUTES_PER_HOUR: u32 = 60;
+
+fn success_rate_by_speed(speed: u8) -> f64 {
+    if speed <= 4 {
+        1.0
+    } else if speed <= 8 {
+        0.9
+    } else {
+        0.77
+    }
+}
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    success_rate_by_speed(speed) * CARS_PER_HOUR * (speed as f64)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) as u32) / MINUTES_PER_HOUR
 }
