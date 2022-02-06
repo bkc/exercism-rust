2b1983f359e959edb0ba71f72cca02ee6d304f6d
Brad Clements <bkc@murkworks.com>
2022-02-06 00:14:31 -0500

    working version, but fails on exercism site due to outdated deps there.

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,28 +1,38 @@
-#[derive(Debug, PartialEq)]
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+
+// Strum contains all the trait definitions
+extern crate strum;
+#[macro_use]
+extern crate strum_macros;
+
+#[repr(u8)]
+#[derive(Debug, Display, Clone, Copy, IntoEnumIterator, IntEnum, PartialEq)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value() as usize
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    let _color: ResistorColor = match ResistorColor::from_int(value as u8) {
+        Ok(value) => value,
+        _ => return String::from("value out of range"),
+    };
+    _color.to_string()
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    ResistorColor::into_enum_iter().collect()
 }
