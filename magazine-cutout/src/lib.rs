d7487aef8bb688aa01e98109427655e17f0a57b4
Brad Clements <bkc@murkworks.com>
2022-02-28 23:44:19 -0500

    add solution to magazine-cutout.

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,9 +1,32 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
-use std::collections::HashMap;
+use std::collections::{hash_map::Entry, HashMap};
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut magazine_map = HashMap::new();
+    for word in magazine.iter() {
+        *magazine_map.entry(word).or_insert(0) += 1;
+    }
+
+    for word in note.iter() {
+        match magazine_map.entry(word) {
+            Entry::Occupied(mut entry) => {
+                *entry.get_mut() -= 1;
+                if *entry.get_mut() < 0 {
+                    return false;
+                }
+            }
+            Entry::Vacant(_) => {
+                return false;
+            }
+        }
+        /* instead of the above match statement I could have used the following method, however
+        I don't like it because it creates a HashMap entry for words that were not in magazine.
+
+        let magazine_counter = magazine_map.entry(word).or_insert(0);
+        *magazine_counter -= 1;
+        if *magazine_counter < 0 {
+            return false;
+        }
+        */
+    }
+    true
 }
