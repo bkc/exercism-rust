e961027f340cb8e8e875f61182dcfd1dfd0ff0c4
Brad Clements <bkc@murkworks.com>
2022-03-27 21:42:31 -0400

    add solution for rpn-calculator

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -8,8 +8,39 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stack: Vec<i32> = Vec::new();
+
+    for element in inputs.iter() {
+        if let CalculatorInput::Value(x) = element {
+            stack.push(*x);
+            continue;
+        }
+        if stack.len() < 2 {
+            return None;
+        }
+        let operand_b = stack.pop().unwrap();
+        let operand_a = stack.pop().unwrap();
+
+        match element {
+            CalculatorInput::Add => {
+                stack.push(operand_a + operand_b);
+            }
+            CalculatorInput::Subtract => {
+                stack.push(operand_a - operand_b);
+            }
+            CalculatorInput::Multiply => {
+                stack.push(operand_a * operand_b);
+            }
+            CalculatorInput::Divide => {
+                stack.push(operand_a / operand_b);
+            }
+            _ => {
+                panic!("unexpected branch")
+            }
+        }
+    }
+    if stack.len() == 1 {
+        return Some(stack[0]);
+    }
+    None
 }
