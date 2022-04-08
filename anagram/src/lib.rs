7f619d0b6ce58079b8298b926a55d9ca410cdd67
Brad Clements <bkc@murkworks.com>
2022-04-08 14:16:18 -0400

    add solution to anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut result: HashSet<&str> = HashSet::new();
+    let lower_word = word.to_lowercase();
+    let mut sorted_word: Vec<char> = lower_word.chars().collect();
+    sorted_word.sort_unstable();
+
+    for possible_anagram in possible_anagrams.iter() {
+        let lower_possible_anagram = possible_anagram.to_lowercase();
+        let mut sorted_possible_anagram: Vec<char> = lower_possible_anagram.chars().collect();
+        sorted_possible_anagram.sort_unstable();
+        if sorted_possible_anagram == sorted_word && lower_possible_anagram != lower_word {
+            result.insert(possible_anagram);
+        }
+    }
+
+    result
 }
