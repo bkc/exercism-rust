83b910141eed7c00ded3b42caba446634cd412c7
Brad Clements <bkc@murkworks.com>
2022-04-09 18:58:33 -0400

    switched to macro to generate planets

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -12,36 +12,29 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! make_planets {
+    ( $($name:ident,$orbital_period:literal);* ) => {
+        $(
+            pub struct $name;
+            impl Planet for $name {
+                fn years_during( d: &Duration) -> f64 {
+                    (d.seconds / SECONDS_PER_EARTH_YEAR) / $orbital_period
+                }
+            }
+        )*
+    };
+}
 
-impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        (d.seconds / SECONDS_PER_EARTH_YEAR) / 0.2408467
-    }
-}
-impl Planet for Venus {}
-impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds / SECONDS_PER_EARTH_YEAR
-    }
-}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+make_planets!(
+    Mercury, 0.2408467;
+    Venus, 0.61519726;
+    Earth, 1.0;
+    Mars, 1.8808158;
+    Jupiter, 11.862615;
+    Saturn, 29.447498;
+    Uranus, 84.016846;
+    Neptune, 164.79132
+);
