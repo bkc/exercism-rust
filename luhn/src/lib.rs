d42213b4342cb374832fe8e23d5b8874b1ff0fc4
Brad Clements <bkc@murkworks.com>
2022-04-10 16:51:36 -0400

    initial solution for luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,28 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let mut is_valid = true;
+    let mut digit_count: i32 = 0;
+    let total: u32 = code
+        .chars()
+        .rev()
+        .filter(|&ch| ch != ' ')
+        .enumerate()
+        .map(|(idx, ch)| {
+            if let Some(val) = ch.to_digit(10) {
+                digit_count += 1;
+                if (idx % 2) == 0 {
+                    val
+                } else if val >= 5 {
+                    val * 2 - 9
+                } else {
+                    val * 2
+                }
+            } else {
+                is_valid = false;
+                0
+            }
+        })
+        .sum();
+
+    (total % 10) == 0 && is_valid && digit_count > 1
 }
