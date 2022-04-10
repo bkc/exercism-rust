83b58dde27d652249cc2d9b6011bbd7f5decd00a
Brad Clements <bkc@murkworks.com>
2022-04-10 15:55:11 -0400

    remove some type casts replace .get() with index access

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -20,28 +20,30 @@ const MINE_ASCII_CODE: u8 = 42;
 const ZERO_ASCII_CODE: u8 = 48;
 
 /// Calculate mine count for given row and column
-fn get_mine_count(minefield: &[&str], current_row: isize, current_column: isize) -> u8 {
+fn get_mine_count(minefield: &[&str], current_row: usize, current_column: usize) -> u8 {
     let mut minecount: u8 = 0;
-    if minefield[current_row as usize].as_bytes()[current_column as usize] == MINE_ASCII_CODE {
+
+    if minefield[current_row].as_bytes()[current_column] == MINE_ASCII_CODE {
         return minecount;
     }
 
     for offset in POINT_OFFSETS {
-        let row_offset = current_row + offset.row;
-        let col_offset = current_column + offset.col;
-        if row_offset < 0 || col_offset < 0 {
+        let row_offset = current_row as isize + offset.row;
+        let col_offset = current_column as isize + offset.col;
+        if row_offset < 0 || col_offset < 0 || row_offset >= minefield.len() as isize {
             continue;
         }
-        if let Some(row) = minefield.get(row_offset as usize) {
-            if let Some(&byte) = row.as_bytes().get(col_offset as usize) {
-                if byte == MINE_ASCII_CODE {
-                    minecount += 1
-                }
-            }
+        let row = minefield[row_offset as usize].as_bytes();
+        if col_offset >= row.len() as isize {
+            continue;
+        }
+        if row[col_offset as usize] == MINE_ASCII_CODE {
+            minecount += 1
         }
     }
     minecount
 }
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let row_count = minefield.len();
     if row_count == 0 {
@@ -51,7 +53,7 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     for (row_index, row) in minefield.iter().enumerate() {
         let mut row_result: Vec<u8> = row.bytes().collect();
         for (column_index, byteval) in row_result.iter_mut().enumerate() {
-            let minecount = get_mine_count(minefield, row_index as isize, column_index as isize);
+            let minecount = get_mine_count(minefield, row_index, column_index);
             if minecount != 0 {
                 *byteval = minecount + ZERO_ASCII_CODE;
             }
