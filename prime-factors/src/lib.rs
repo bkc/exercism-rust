586f247c690e9c8ed68a783cc5ebc0daface8c44
Brad Clements <bkc@murkworks.com>
2022-04-14 14:48:15 -0400

    add solution for factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut remaining = n;
+    let mut prime_factors: Vec<u64> = Vec::new();
+    let mut divisor: u64 = 2;
+
+    while divisor <= n && remaining > 1 {
+        if remaining % divisor != 0 {
+            divisor += 1;
+            continue;
+        }
+        prime_factors.push(divisor);
+        remaining /= divisor;
+    }
+    prime_factors
 }
