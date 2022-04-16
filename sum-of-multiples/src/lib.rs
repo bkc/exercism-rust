8f1a0cfbd091b18f0dce6ef6305aac5d9eded053
Brad Clements <bkc@murkworks.com>
2022-04-16 15:09:21 -0400

    add solution for sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit)
+        .filter(|value| {
+            factors
+                .iter()
+                .any(|&factor| factor != 0u32 && value % factor == 0)
+        })
+        .sum()
 }
