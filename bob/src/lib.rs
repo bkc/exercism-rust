97300b78e571fe798e358001f973f02e08bd4884
Brad Clements <bkc@murkworks.com>
2022-04-16 17:13:08 -0400

    add solution to bob

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let trim_message = message.trim_end();
+    let is_question = trim_message.ends_with('?');
+    let is_yell = trim_message.chars().filter(|ch| ch.is_alphabetic()).count() > 0
+        && trim_message
+            .chars()
+            .filter(|ch| ch.is_alphabetic())
+            .all(|ch| ch.is_uppercase());
+    match (is_question, is_yell, trim_message) {
+        (true, true, _) => "Calm down, I know what I'm doing!",
+        (false, true, _) => "Whoa, chill out!",
+        (true, false, _) => "Sure.",
+        (false, false, "") => "Fine. Be that way!",
+        _ => "Whatever.",
+    }
 }
