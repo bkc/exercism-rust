2bea24859454fb3e3e96caef113bc32aaf68702d
Brad Clements <bkc@murkworks.com>
2022-04-16 14:00:33 -0400

    initial solution to proverb using for loop

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,18 @@
+use std::str::FromStr;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::from_str("").unwrap();
+    }
+    let mut previous_item = list[0];
+    let mut result: Vec<String> = list[1..]
+        .iter()
+        .map(|&item| {
+            let result = format!("For want of a {} the {} was lost.", previous_item, item);
+            previous_item = item;
+            result
+        })
+        .collect();
+    result.push(format!("And all for the want of a {}.", list[0]));
+    result.join("\n")
 }
