a170a613fc51c29d7b9d0df3ed2fe5abd8be07e6
Brad Clements <bkc@murkworks.com>
2022-04-26 23:13:57 -0400

    add solution for high-scores

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -8,29 +8,33 @@ const MAXIMUM_HIGHEST_SCORES: usize = 3;
 
 impl<'a> HighScores<'a> {
     pub fn new(scores: &'a [u32]) -> Self {
+        let maximum_result_length = scores.len().min(MAXIMUM_HIGHEST_SCORES);
+
         HighScores {
             scores,
             highest_scores: scores.iter().fold(
-                Vec::new() as Vec<u32>,
+                Vec::with_capacity(MAXIMUM_HIGHEST_SCORES + 1),
                 |mut highest_scores, item| {
                     if highest_scores.is_empty() {
-                        // length 0
-                        highest_scores.push(*item)
-                    } else if highest_scores.len() < MAXIMUM_HIGHEST_SCORES {
-                        // length 2
-                        if highest_scores[0] <= *item {
-                            highest_scores.insert(0, *item)
-                        } else {
-                            highest_scores.push(*item)
+                        highest_scores.push(*item);
+                    } else if highest_scores.last().unwrap() <= item
+                        || highest_scores.len() < maximum_result_length
+                    {
+                        // temporarily add 0 to end of vec
+                        if highest_scores.len() < MAXIMUM_HIGHEST_SCORES {
+                            highest_scores.push(0);
                         }
-                    } else if highest_scores[0] <= *item {
-                        highest_scores.insert(0, *item);
-                        highest_scores.truncate(MAXIMUM_HIGHEST_SCORES)
-                    } else if highest_scores[1] <= *item {
-                        highest_scores.insert(1, *item);
-                        highest_scores.truncate(MAXIMUM_HIGHEST_SCORES)
-                    } else if highest_scores[2] < *item {
-                        highest_scores[2] = *item;
+                        // insert new item in correct location, that moves added 0 to the right
+                        for idx in 0..highest_scores.len() {
+                            if highest_scores[idx] <= *item {
+                                highest_scores.insert(idx, *item);
+                                break;
+                            }
+                        }
+                        // truncate vec to max length
+                        if highest_scores.len() > maximum_result_length {
+                            highest_scores.truncate(maximum_result_length)
+                        }
                     }
                     highest_scores
                 },
